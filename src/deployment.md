4714674beee81e1f447513dcccee21f32de6911b
carlo <carlogbossou93@gmail.com>
2026-05-30 14:11:55 +0000

    Configuration of file duplication detection

--- src/deployment.md
+++ src/deployment.md
@@ -1,10 +1,20 @@
+-- Deploiement
+
 aws cloudformation package `
   --template-file infrastructure\template.yaml `
   --s3-bucket iabd-sourcecode-management-bucket `
   --output-template-file infrastructure\packaged.yaml
 
-
-  aws cloudformation deploy `
+aws cloudformation deploy `
   --template-file infrastructure\packaged.yaml `
   --stack-name fgbossou-lambda-s3-trigger-stack `
-  --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
+  --capabilities CAPABILITY_IAM `
+  --no-cli-pager
+
+
+  -- Supprimer la stack
+  aws cloudformation delete-stack --stack-name fgbossou-lambda-s3-trigger-stack
+  aws cloudformation wait stack-delete-complete --stack-name fgbossou-lambda-s3-trigger-stack
+
+
+  learningesgissandbox
--- src/main.py
+++ src/main.py
@@ -1,11 +1,45 @@
 import json
+import boto3
+import urllib.parse
+import logging
+from botocore.exceptions import ClientError
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('s3-trigger-db')
 
 def lambda_handler(event, context):
-    print("Event: ", json.dumps(event))
-    return {
-    'statusCode': 200,
-    'body': json.dumps('Hello from Lambda loaded from src/main.py!')
-     }
+    print("Event:", json.dumps(event))
 
+    record = event['Records'][0]
+    bucket = record['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(record['s3']['object']['key'])
+
+    logger.info(f"Fichier reçu : s3://{bucket}/{key}")
+
+    try:
+        table.put_item(
+            Item={
+                'PK': key,        # ✅ correspond à la partition key de ta table
+                'bucket': bucket,
+            },
+            ConditionExpression='attribute_not_exists(PK)'  # ✅ même nom
+        )
+        logger.info(f"✅ Nouveau fichier enregistré : {key}")
+        return {
+            'statusCode': 200,
+            'body': json.dumps(f'Fichier traité : {key}')
+        }
+
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'ConditionalCheckFailedException':
+            logger.warning(f"⛔ Doublon détecté, ignoré : {key}")
+            return {
+                'statusCode': 409,
+                'body': json.dumps(f'Fichier déjà traité : {key}')
+            }
+        raise
 
 
